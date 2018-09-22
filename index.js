eb1779bbc4a3dcf330dcad28c6856746c0926ef4
Nkanyiso Ndlovu <kndlovun@gmail.com>
2018-09-21 23:07:38 -0400

    add flash intro

--- index.js
+++ index.js
@@ -8,6 +8,15 @@ var textBox = document.getElementById('textBox');
 
 var tsquare = '';
 
+
+ window.onload = function(){
+ 	flashAnimation();
+ 	setTimeout(function(){
+ 		resetBoard();
+ 	}, 3500);
+
+ }
+
 //Show or hide answers
 btnShowHide.addEventListener('click', function(){
     for(var i = 0; i < squares.length; i++){
@@ -18,7 +27,7 @@ btnShowHide.addEventListener('click', function(){
 //Select random square to guess
 btnSelectRanSquare.addEventListener('click', function(){
 
-		for(var i = 0; i < squares.length; i++){
+	for(var i = 0; i < squares.length; i++){
 		squares[i].style.backgroundColor = "#8b4513";
 		squares[i].style.color = "#8b4513";
 	}
@@ -40,7 +49,6 @@ btnReset.addEventListener('click', function(){
 	message.textContent = "";
 	textBox.value = "";
 	tsquare.style.color = "#8b4513";
-
 });
 
 //Entering the guess and grading logic
@@ -59,3 +67,35 @@ function randomNum(){
 	var num = Math.floor(Math.random() * (65 - 1)) + 1;
 	return num;
 }
+
+function RanSquarePicker(){
+	for(var i = 0; i < squares.length; i++){
+		squares[i].style.backgroundColor = "#8b4513";
+		squares[i].style.color = "#8b4513";
+	}
+
+	tsquare = document.getElementById(randomNum().toString());
+	tsquare.style.color = "Green";
+	console.log(tsquare.textContent);
+	tsquare.style.backgroundColor = "Green";
+
+}
+
+function resetBoard(){
+	for(var i = 0; i < squares.length; i++){
+		squares[i].style.backgroundColor = "#8b4513";
+		squares[i].style.color = "#8b4513";
+	}
+}
+
+function flashAnimation(){
+  	var timesRun = 0;
+    var interval = setInterval(function(){
+    	timesRun += 1;
+    	if(timesRun === 30){
+    		clearInterval(interval);
+    	}
+    	RanSquarePicker();
+    }, 100); 
+};
+
